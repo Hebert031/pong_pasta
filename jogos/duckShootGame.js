5085910ca49fe015c1019299a165183bb41dac33
Hebert031 <hebertribeiro31@gmail.com>
2024-03-13 04:57:44 -0300

    Atualizações no jogo Duck Shoot

--- jogos/duckShootGame.js
+++ jogos/duckShootGame.js
@@ -13,6 +13,9 @@ function init() {
 
     // Obtém uma referência para o pato
     var duck = document.getElementById('duck');
+    // Obtém o tamanho do pato
+    var duckWidth = duck.offsetWidth;
+    var duckHeight = duck.offsetHeight;
 
     // Inicializa a pontuação
     var score = 0;
@@ -22,9 +25,9 @@ function init() {
 
     // Move o pato para uma posição aleatória a cada segundo
     setInterval(function() {
-        duck.style.top = Math.random() * window.innerHeight + 'px';
-        duck.style.left = Math.random() * window.innerWidth + 'px';
-    }, 1000);
+        duck.style.top = Math.random() * (window.innerHeight - duckHeight) + 'px';
+        duck.style.left = Math.random() * (window.innerWidth - duckWidth) + 'px';
+    }, 1800);
 
     // Adiciona um evento de clique ao pato
     duck.addEventListener('click', function(event) {
@@ -36,6 +39,41 @@ function init() {
 
         // Atualiza a pontuação na tela
         scoreElement.textContent = 'Pontuação: ' + score;
+        // Atualiza o elemento de pontuação
+        scoreElement.textContent = score;
+
+// Se a pontuação for um múltiplo de 50, mostra uma mensagem e toca um beep
+if (score % 4 === 0) {
+    // Cria um novo elemento para a mensagem
+var message = document.createElement('div');
+message.style.position = 'fixed';
+message.style.top = '50%';
+message.style.width = '100%';
+message.style.textAlign = 'center';
+message.style.backgroundColor = 'yellow';
+message.style.position = 'fixed';
+message.style.top = '80%';
+message.style.left = '80%';
+message.style.transform = 'translate(-50%, -50%)';
+message.style.width = '100px'; // Ajuste para o tamanho desejado
+message.style.height = '100px'; // Deve ser igual à largura para um quadrado
+
+
+// Cria um novo elemento de imagem para o GIF
+var gif = document.createElement('img');
+gif.src = '../midia/duck3.gif';
+
+// Adiciona o GIF ao div
+message.appendChild(gif);
+
+// Adiciona o div ao corpo do documento
+document.body.appendChild(message);
+
+// Remove a mensagem após 30 segundos
+setTimeout(function() {
+    document.body.removeChild(message);
+}, 1500);
+}
 
         // Verifica se a pontuação é 300
         if (score >= 300) {
